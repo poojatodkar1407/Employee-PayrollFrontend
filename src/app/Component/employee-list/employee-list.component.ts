a3e53a6c9ea9d25382f887d5c5ebae73d2203b2f
poojatodkar1407 <poojatodkar124@gmail.com>
2020-12-18 13:16:37 +0530

    Code updated

--- src/app/Component/employee-list/employee-list.component.ts
+++ src/app/Component/employee-list/employee-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { UserService } from '../../Services/EmployeeService/user.service'
 import { Employee } from "../../Employee";
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-employee-list',
@@ -19,67 +18,67 @@ export class EmployeeListComponent implements OnInit {
   }
   @ViewChildren('checkBox') checkBox: QueryList<any>;
   employee: Employee = new Employee();
-  id:any;
+  id: any;
+  a: boolean = false;
   submitted = false;
   userDetail: FormGroup;
   checked = [];
   precio = 0;
-  employees:any;
-  isEdit: Boolean = false;
+  employees: any;
+  isEdit: Boolean;
+  departments: string[] =[]
   department = ['Hr', 'Sales', 'Finance', 'Engineer', 'Other'];
   constructor(private employeeService: UserService,
     private formBuilder: FormBuilder, private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit() {
+
     this.userDetail = this.formBuilder.group({
       name: [null, Validators.compose([Validators.required, Validators.minLength(2),
       Validators.pattern('[a-zA-Z ]*$')])],
       salary: [null, Validators.required],
-      department:[null, Validators.required],
+      dept: [null, Validators.required],
       gender: [null, Validators.required],
       day: [null, Validators.required],
       year: [null, Validators.required],
-      month: [null, Validators.required]
-    
+      month: [null, Validators.required],
+      department:[null, Validators.required]
     });
 
-    // this.route.params.subscribe(param => {
-    //   console.log(param)
-    //   if (param && param.id) {
-    //     console.log("inside if")
-    //     this.employeeService.getEmployee(param.id).subscribe((response:any) =>{
-    //       this.id = param.id;
-    //       this.isEdit = true;
-    //       this.userDetail.controls["name"].setValue(response.name)
-    //       this.userDetail.controls["salary"].setValue(response.salary)
-    //       this.userDetail.controls["gender"].setValue(response.gender)
-    //      var str = response.startDate;
-    //     var splited :[0,1,2] = str.split(" ");  
-    //     console.log("splitted is",splited)
-    //     var checked1 = response.department;
-    //     var array : [0,1,2,3,4,5] = checked1.split(",");
-    //       console.log("checked",array)
-    //     this.userDetail.controls["day"].setValue(splited[0])
-    //     this.userDetail.controls["month"].setValue(splited[1])
-    //     this.userDetail.controls["year"].setValue(splited[2])
-    //     this.userDetail.controls["department"].setValue(array[0])
+    this.route.params.subscribe(param => {
+      console.log(param)
+      if (param && param.id) {
+        console.log("inside if")
+        this.employeeService.getEmployee(param.id).subscribe((response: any) => {
+          console.log(response)
+          this.id = param.id;
+          this.isEdit = true;
+          this.userDetail.controls["name"].setValue(response.name)
+           this.userDetail.controls["salary"].setValue(response.salary)
+           this.userDetail.controls["gender"].setValue(response.gender)
+           var str = response.startDate;
+           var splited: [0, 1, 2] = str.split(" ");
+          // console.log("splitted is", splited)
+           this.departments = response.departments;         
+          console.log("department", this.departments)
+          this.userDetail.controls["day"].setValue(splited[0])
+          this.userDetail.controls["month"].setValue(splited[1])
+          this.userDetail.controls["year"].setValue(splited[2])
 
-    //     })
-        
-    //   }
-
-    // })
-
+        })
+      }
+    })
   }
 
-  getCheckbox(checkbox) {
+  getCheckbox() {
     this.checked = [];
     const checked = this.checkBox.filter(checkbox => checkbox.checked);
     checked.forEach(data => {
       this.checked.push(data.value
       )
     })
+    console.log(this.checked)
   }
 
   getPrecio(event) {
@@ -157,24 +156,27 @@ export class EmployeeListComponent implements OnInit {
     this.employeeService.createEmployee(employeeDto).subscribe((response: any) => {
       console.log("response is " + response.message
       );
-this.router.navigate(["/"]);
-      console.log("response",response.object)
+      this.router.navigate(["/"]);
+      console.log("response", response.object)
     })
   }
 
-update(){
-    // var employeeDto = {
-    //   'id': this.id,
-    //   'name': this.userDetail.controls['name'].value,
-    //   'salary': this.precio,
-    //   'department': this.checked.toString(),
-    //   'gender': this.userDetail.controls['gender'].value,
-    //   'startDate': this.userDetail.controls['day'].value + " " + this.userDetail.controls['month'].value + " " + this.userDetail.controls['year'].value
-    // };
+  update() {
+    console.log(this.userDetail.controls['gender'].value+" "+this.userDetail.controls['month'].value)
+    var employeeDto = {
+      'id': this.id,
+      'name': this.userDetail.controls['name'].value,
+      'salary': this.precio,
+      'departments': this.checked,
+      'gender': this.userDetail.controls['gender'].value,
+      'startDate': this.userDetail.controls['day'].value + " " + this.userDetail.controls['month'].value + " " + this.userDetail.controls['year'].value
+    };
 
-    // this.employeeService.updateEmployee(employeeDto).subscribe((response: any) => {
-    //   console.log("response is " + response);
-    // })
+    this.employeeService.updateEmployee(employeeDto).subscribe((response: any) => {
+      console.log("response is " + response);
+      this.router.navigate(["/"]);
+
+    })
   }
 
   onSubmit() {
@@ -182,9 +184,11 @@ update(){
     this.register();
   }
 
-  reset(){
+  reset() {
     this.userDetail.reset();
   }
 
-
+  toggleCheckBox(dept){
+    return (this.departments.includes(dept)) ? true : false;
+ }
 }
